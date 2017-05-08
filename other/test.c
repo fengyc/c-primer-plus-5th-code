f4bd6e3388d0ccb4ede8e77b4496c95a33e7d7a9
fengyingcai <fengyc.work@gmail.com>
2017-05-08 21:57:42 +0800

    update

--- other/test.c
+++ other/test.c
@@ -1,44 +1,88 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
-int gcd(int x, int y)
+// 数字键盘，最少为 10、20、33、40、50、66、70、80、99
+int pad[4][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {-1, 0, -1}
+};
+int pad_min[] = {10, 20, 33, 40, 50, 66, 70, 80, 99};
+
+
+void printMaxK(char *s)
 {
-    int k = x % y;
-    if (k==0){
-        return y;
+    int len = strlen(s);
+    int i, j;   // 当前位置
+    int k, l;
+    int delta, min_delta, m, n;  // 与数字最接近的位置
+    int flag;   // 如果输出的数已少于 s
+    while (s[0] != 0 && s[0]-'0' == 0) {
+        s++;
+        len--;
     }
-    return gcd(y, k);
-}
-
-char r[100][100];
-
-int main(void) {
-
-    int w, x, y, z;
-    int p, q;
-    int k;
-    int count=0;
-
-    memset(r, 0, 100*100);
-
-    scanf("%d %d %d %d", &w, &x, &y, &z);
-
-    for (p=w; p<=x; p++)
-    {
-        for(q=y; q<=z; q++)
-        {
-            if (p>q)
-                k = gcd(p, q);
-            else
-                k=gcd(q,p);
-            if (r[p/k][q/k]==0) {
-                count++;
-                r[p/k][q/k] = 1;
+    if (s[0] == 0 || s[0]=='0')
+        printf("0\n");
+    else {
+        // 首个位置，确定从 s[0] 还是 s[0]-1 开始
+        flag = 0;
+        if (len >=2 ){
+            if ((s[0]-'0')*(s[1]-'0') < (pad_min[s[0]-'0'-1]))
+            {
+                flag = 1;
+                i = (s[0]-'0'-2)/3;
+                j = (s[0]-'0'-2)%3;
             }
         }
+        i = (s[0] - '0' - 1 - flag) / 3;
+        j = (s[0] - '0' - 1 - flag) % 3;
+        printf("%d", pad[i][j]);
+        s++;
+        while (s[0]!=0){
+            if (flag) {
+                printf("9");
+            }
+            // 找一个最近的数
+            else{
+                min_delta = -99;
+                for (k=i; k < 3; k++){
+                    for (l=j;l<4;l++) {
+                        delta = (s[0]-'a') - pad[i][j];
+                        if (min_delta < 0 || (delta >=0 && delta < min_delta)) {
+                            min_delta = delta;
+                            m=k;
+                            n=l;
+                        }
+                    }
+                }
+                if (min_delta > 0){
+                    flag = 1;
+                }
+                i=m;
+                j=n;
+                printf("%d", pad[i][j]);
+            }
+            s++;
+        }
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    char s[550];
+    int t;
+
+    scanf("%d", &t);
+    while(t--){
+        scanf("%s", s);
+        printMaxK(s);
     }
 
-    printf("%d\n", count);
     return 0;
 }
+
 
--- other/testkk.cpp
+++ other/testkk.cpp
@@ -1,119 +1,23 @@
-#include <deque>
-#include <cstdio>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int calc(int a, int b, char op){
-    switch (op) {
-        case '+':
-            return a+b;
-        case '-':
-            return a-b;
-        case '*':
-            return a*b;
-    }
-    return 0;
-}
+// 数字键盘
+int pad[4][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {-1, 0, -1}
 
-char get_order(char op)
-{
-    switch(op){
-        case '(':
-        case ')':
-            return 0;
-        case '*':
-            return -1;
-        case '+':
-        case '-':
-            return -2;
-    }
-    return 0;
-}
-
-// 操作符优先
-char op_order(char op1, char op2)
-{
-    return get_order(op1) - get_order(op2);
-}
-
-void pop_calc(deque<char>* operators, deque<int>* oprands)
-{
-    char op;
-    int k0, k1;
-    int r;
-
-    op = *operators->rbegin();
-    operators->pop_back();
-    k0 = *oprands->rbegin();
-    oprands->pop_back();
-    k1 = *oprands->rbegin();
-    oprands->pop_back();
-    r = calc(k1, k0, op);
-    oprands->push_back(r);
-}
+};
 
 int main(void)
 {
-    char s[4000];
-    char *p;
-    int num;
-    char op;
-    int k0, k1;
-    int r;
-    deque<char> operators;
-    deque<int> oprands;
-    int i;
-    int k;
 
-    scanf("%s", s);
-    p = s;
-    while (*p!=0) {
-        if (*p>='0' && *p<='9') {
-            // 数字处理
-            k = sscanf(p, "%d", &num);
-            oprands.push_back(num);
-            while(*p>='0' && *p<='9')
-                p++;
-        }
-        else {
 
-            // 操作符处理
-            if (*p=='(') {
-                operators.push_back(*p);
-                p++;
-            }
-            else if (*p==')') {
-                while(*operators.rbegin()!='(') {
-                    pop_calc(&operators, &oprands);
-                }
-                // 退 (
-                operators.pop_back();
-                p++;
-            }
-            else {
-                // *p 优先级计算
-                while (operators.rbegin() != operators.rend() && *operators.rbegin()!='(' &&
-                        op_order(*operators.rbegin(), *p)>=0) {
-                    pop_calc(&operators, &oprands);
-                }
-                // 压符号
-                operators.push_back(*p);
-                p++;
-            }
-        }
-    }
-
-    // 最后的计算
-    while(operators.rbegin() != operators.rend())
-    {
-        pop_calc(&operators, &oprands);
-    }
-
-    r = *oprands.rbegin();
-    oprands.empty();
-    operators.empty();
-
-    printf("%d\n", r);
 
     return 0;
 }
